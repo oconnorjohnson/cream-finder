d351d18acfe09d1771e1bf9a9e7fbf5381639fd2
marvinsantiago <marvinsantiago@MindYouBusiness.lan>
2022-07-11 13:33:45 -0700

    routeRoutes.js and fixed routes

---
+++ controllers/api/routeRoutes.js
@@ -0,0 +1,38 @@
+const router = require('express').Router();
+const { Route } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const newRoute = await Route.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newRoute);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const routeData = await Route.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!routeData) {
+      res.status(404).json({ message: 'No route found with this id!' });
+      return;
+    }
+
+    res.status(200).json(routeData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
