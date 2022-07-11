f05986af1d0bf9ee2cf9550ab48aa5f13d5edb82
prsoulman <60936175+prsoulman@users.noreply.github.com>
2022-07-11 09:30:21 -0500

    hbs

--- seeds/index.js
+++ seeds/index.js
@@ -1,13 +1,13 @@
 const sequelize = require('../config/connection');
 const seedUser = require('./UserData');
-//const seedTruck = require('./TruckData');
+const seedTruck = require('./TruckData');
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
 
   await seedUser();
 
-  //await seedTruck();
+  await seedTruck();
 
   process.exit(0);
 };
