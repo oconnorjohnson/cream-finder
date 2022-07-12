227cc28e5f6c31ba5290845566bdf4cc2eb62535
thaflavorsaver <dojohnson@ucdavis.edu>
2022-07-12 16:45:04 -0700

    final seed changes for posting routes to driver profile

--- seeds/routeData.js
+++ seeds/routeData.js
@@ -2,30 +2,105 @@ const { Route } = require('../models');
 
 const Routedata = [
   {
-    address: '4659 Crystal Springs Dr, Los Angeles, CA 90027',
+    address: 'Crystal Springs Picnic Area, 4659 Crystal Springs Dr, Los Angeles, CA 90027, ETA: 11:00 AM',
     order: 1,
     user_id: 1
   },
   {
-    address: '1900 N San Fernando Rd, Los Angeles, CA 90065',
+    address: 'Rio De Los Angeles State Park, 1900 N San Fernando Rd, Los Angeles, CA 90065, ETA: 12:30 PM',
     order: 2,
     user_id: 1
   },
   {
-    address: '1245 N Spring St, Los Angeles, CA 90012',
+    address: 'Los Angeles State Historic Park, 1245 N Spring St, Los Angeles, CA 90012, ETA: 2:00 PM',
     order: 3,
     user_id: 1
   },
   {
-    address: '1900 N San Fernando Rd, Los Angeles, CA 90065',
+    address: 'MacArthur Park, 2230 W 6th St, Los Angeles, CA 90057, ETA: 4:00 PM',
+    order: 4,
+    user_id: 1
+  },
+  {
+    address: 'Lafayette Park, 5077006901, Los Angles, CA 90005, ETA: 5:30 PM',
+    order: 5,
+    user_id: 1
+  },
+  {
+    address: 'Hollywood Forever Cemetery, 6000 Santa Monica Blvd, Los Angeles, CA 90038, ETA: 10:00 AM',
     order: 1,
     user_id: 2
   },
   {
-    address: '1245 N Spring St, Los Angeles, CA 90012',
+    address: 'Hancock Park, 5800 Block of Wilshire Blvd, Miracle Mile, Los Angeles, CA, ETA: 11:30 AM',
     order: 2,
     user_id: 2
-  }
+  },
+  {
+    address: 'Poinsettia Recreation Center, 7341 Willoughby Ave, Los Angeles, CA 90046, ETA: 1:00 PM',
+    order: 3,
+    user_id: 2
+  },
+  {
+    address: 'Playground Pavillion, SOuth La Brea Ave, Los Angeles, CA 90036, ETA: 2:30 PM',
+    order: 4,
+    user_id: 2
+  },
+  {
+    address: 'Fairfax High School, 7850 Melrose Ave, Los Angeles, CA 90046, ETA: 3:30 PM',
+    order: 5,
+    user_id: 2
+  },
+  {
+    address: 'The Los Angeles Country Club, 10101 Wilshire Blvd, Los Angeles, CA 90024, ETA: 11:30 AM',
+    order: 1,
+    user_id: 3
+  },
+  {
+    address: 'Santa Monica Pier, 200 Santa Monica Pier, Santa Monica, CA 90401, ETA: 12:30 PM',
+    order: 2,
+    user_id: 3
+  },
+  {
+    address: 'Venice Beach, Venice, CA 90261, ETA: 2:00 PM',
+    order: 3,
+    user_id: 3
+  },
+  {
+    address: 'Santa Monica College, 1900 Pico Blvd, Santa Monica, CA 90405, ETA: 3:30 PM',
+    order: 4,
+    user_id: 3
+  },
+  {
+    address: 'Burton Chase Park, 13650 Mindanao Way, Marina Del Rey, CA 90292, ETA: 5:00 PM',
+    order: 5,
+    user_id: 3
+  },
+  {
+    address: 'Playa Del Rey Beach, 7313-7351 S Marine Ave, Playa Del Rey, CA 90263, ETA: 10:00 AM',
+    order: 1,
+    user_id: 4
+  },
+  {
+    address: 'Fox HIlls Park, Green Valley Cir &, Buckingham Pkwy, Culver City, CA 90230, ETA: 12:00 PM',
+    order: 2,
+    user_id: 4
+  },
+  {
+    address: 'Kenneth hahn State Recreation Area, 4100 S La Cienega Blvd, Los Angeles, CA 90056, ETA: 2:00 PM',
+    order: 3,
+    user_id: 4
+  },
+  {
+    address: 'Los Angeles County Museum of Art, 5905 Wilshire Blvd, Los Angeles, CA 90036, ETA: 3:00 PM',
+    order: 4,
+    user_id: 4
+  },
+  {
+    address: 'UCLA, Los Angeles, CA 90095, ETA: 4:30 PM',
+    order: 5,
+    user_id: 4
+  },
 ];
 
 // const Routedata = [
