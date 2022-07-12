c76af34e5032dd42b60c5162ff8142d99ccfde86
marvinsantiago <marvinsantiago@MindYouBusiness.lan>
2022-07-12 14:05:00 -0700

    login and index

--- public/js/login.js
+++ public/js/login.js
@@ -16,7 +16,7 @@ const loginFormHandler = async (event) => {
   
       if (response.ok) {
         // If successful, redirect the browser to the profile page
-        document.location.replace('truckprofile');
+        document.location.replace('/truckprofile');
         console.log(response);
       } else {
         alert(response.statusText);
