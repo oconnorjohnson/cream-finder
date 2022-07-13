aa7d2e5d78b751ee64114da4d29ce3e58f321088
marvinsantiago <marvinsantiago@MindYouBusiness.lan>
2022-07-13 12:22:14 -0700

    post works

--- controllers/api/routeRoutes.js
+++ controllers/api/routeRoutes.js
@@ -2,11 +2,11 @@ const router = require('express').Router();
 const { Route } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-router.post('/', withAuth, async (req, res) => {
+router.post('/', async (req, res) => {
   try {
     const newRoute = await Route.create({
       ...req.body,
-      user_id: req.session.user_id,
+      // route_id: req.session.user_id,
     });
 
     res.status(200).json(newRoute);
